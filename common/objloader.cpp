c98cf67aed4e53faa497ccfa3af43a1e3908d823
sayakbiswas <sayak90@gmail.com>
2017-03-17 12:20:00 -0400

    Added functions to load quads in the utility files

--- common/objloader.cpp
+++ common/objloader.cpp
@@ -96,3 +96,81 @@ bool loadOBJ(
 
 	return true;
 }
+
+bool loadQuadOBJ(const char * path, std::vector<glm::vec3> &out_vertices, std::vector<glm::vec3> &out_normals) {
+    printf("Loading OBJ file with quads %s...\n", path);
+
+    std::vector<unsigned int> vertexIndices, normalIndices;
+    std::vector<glm::vec3> temp_vertices;
+    std::vector<glm::vec3> temp_normals;
+
+
+    FILE * file = fopen(path, "r");
+    if( file == NULL ){
+        printf("Impossible to open the file ! Are you in the right path ? See Tutorial 1 for details\n");
+        getchar();
+        return false;
+    }
+
+    while( 1 ){
+
+        char lineHeader[128];
+        // read the first word of the line
+        int res = fscanf(file, "%s", lineHeader);
+        if (res == EOF)
+            break; // EOF = End Of File. Quit the loop.
+
+        // else : parse lineHeader
+
+        if ( strcmp( lineHeader, "v" ) == 0 ){
+            glm::vec3 vertex;
+            fscanf(file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z );
+            temp_vertices.push_back(vertex);
+        }else if ( strcmp( lineHeader, "vn" ) == 0 ){
+            glm::vec3 normal;
+            fscanf(file, "%f %f %f\n", &normal.x, &normal.y, &normal.z );
+            temp_normals.push_back(normal);
+        }else if ( strcmp( lineHeader, "f" ) == 0 ){
+            std::string vertex1, vertex2, vertex3, vertex4;
+            unsigned int vertexIndex[4], normalIndex[4];
+            int matches = fscanf(file, "%d//%d %d//%d %d//%d %d//%d\n", &vertexIndex[0], &normalIndex[0], &vertexIndex[1], &normalIndex[1], &vertexIndex[2], &normalIndex[2], &vertexIndex[3], &normalIndex[3] );
+            if (matches != 8){
+                printf("ERROR: NO NORMALS PRESENT IN FILE! YOU NEED NORMALS FOR LIGHTING CALCULATIONS!\n");
+                printf("File can't be read by our simple parser :-( Try exporting with other options. See the definition of the loadOBJ fuction.\n");
+                return false;
+            }
+            vertexIndices.push_back(vertexIndex[0]);
+            vertexIndices.push_back(vertexIndex[1]);
+            vertexIndices.push_back(vertexIndex[2]);
+            vertexIndices.push_back(vertexIndex[3]);
+            normalIndices.push_back(normalIndex[0]);
+            normalIndices.push_back(normalIndex[1]);
+            normalIndices.push_back(normalIndex[2]);
+            normalIndices.push_back(normalIndex[3]);
+        }else{
+            // Probably a comment, eat up the rest of the line
+            char stupidBuffer[1000];
+            fgets(stupidBuffer, 1000, file);
+        }
+
+    }
+
+    // For each vertex of each quad
+    for( unsigned int i=0; i<vertexIndices.size(); i++ ){
+
+        // Get the indices of its attributes
+        unsigned int vertexIndex = vertexIndices[i];
+        unsigned int normalIndex = normalIndices[i];
+
+        // Get the attributes thanks to the index
+        glm::vec3 vertex = temp_vertices[ vertexIndex-1 ];
+        glm::vec3 normal = temp_normals[ normalIndex-1 ];
+
+        // Put the attributes in buffers
+        out_vertices.push_back(vertex);
+        out_normals .push_back(normal);
+
+    }
+
+    return true;
+}
--- common/objloader.hpp
+++ common/objloader.hpp
@@ -16,4 +16,6 @@ bool loadAssImp(
 	std::vector<glm::vec3> & normals
 );
 
+bool loadQuadOBJ(const char * path, std::vector<glm::vec3> &out_vertices, std::vector<glm::vec3> &out_normals);
+
 #endif
